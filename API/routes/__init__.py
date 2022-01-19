3e61943d468bd923c05d57434ed62a9f90e53156
Programmer-RD-AI <aigithub@gmail.com>
2022-01-19 22:55:48 +0530

    Create __init__.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ API/routes/__init__.py
@@ -0,0 +1,5 @@
+from API.routes.accounts import *
+from API.routes.azure_sql_api import *
+from API.routes.azure_storage_api import *
+from API.routes.contact_us import *
+from API.routes.get_config import *
