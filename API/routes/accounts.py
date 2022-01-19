d2d4e9fdf03e5aebac69ed9bd2a69d24dbb585be
Programmer-RD-AI <aigithub@gmail.com>
2022-01-19 22:55:53 +0530

    Create accounts.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ API/routes/accounts.py
@@ -0,0 +1,66 @@
+from API import *
+
+hp = Help_Funcs()
+accounts_request_parser = reqparse.RequestParser()
+accounts_request_parser.add_argument(
+    """email""", type=str, help="""email is required""", required=True
+)
+accounts_request_parser.add_argument(
+    """password""", type=str, help="""Password is required""", required=True
+)
+accounts_request_parser.add_argument(
+    """user_name""", type=str, help="""user name is required""", required=True
+)
+accounts_request_parser.add_argument("""password_hash""", type=str, required=True)
+
+
+class Accounts(Resource):
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+
+    def get(self) -> dict:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        asql = Azure_SQL()
+        hp.table_exists_or_not(
+            """Accounts""",
+            """CREATE TABLE Accounts (ID int IDENTITY(1,1), Rank INT(max), Email varchar(max),User_Name varchar(max), Password varchar(max))""",
+        )
+        newaccounts = []
+        accounts = asql.select_table("""SELECT * FROM [Accounts]""")
+        for account in accounts:
+            newaccounts.append(list(account))
+        return {"""message""": newaccounts}
+
+    def post(self) -> dict:
+        """sumary_line
+
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        args = accounts_request_parser.parse_args()
+        asql = Azure_SQL()
+        hp.table_exists_or_not(
+            """Accounts""",
+            """CREATE TABLE Accounts (ID int IDENTITY(1,1), Rank INT(max), Email varchar(max),User_Name varchar(max), Password varchar(max))""",
+        )
+        asql.insert_to_table(
+            f"""INSERT INTO [Accounts]( [Rank],[Email], [User_Name], [Password] ) VALUES ( 1,'{args['email']}', '{args['user_name']}', '{args['password']}')"""
+        )
+        newaccounts = []
+        accounts = asql.select_table("""SELECT * FROM [Accounts]""")
+        for account in accounts:
+            newaccounts.append(list(account))
+        return {"""message""": newaccounts}
+
+
+api.add_resource(Accounts, """/api/Accounts""")
