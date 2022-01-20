e8ad1470bc5401d6cf183fe228b766e7f4970a68
Programmer-RD-AI <aigithub@gmail.com>
2022-01-20 10:45:13 +0530

    Update SQLQuery.sql

--- SQL/SQLQuery.sql
+++ SQL/SQLQuery.sql
@@ -20,13 +20,7 @@ FROM
 -- Accounts
 DROP TABLE Accounts;
 
-CREATE TABLE Accounts (
-    [ID] int IDENTITY(1, 1),
-    Rank INT,
-    [Email] varchar(max),
-    [User_Name] varchar(max),
-    [Password] varchar(max)
-);
+CREATE TABLE Accounts (ID int IDENTITY(1,1), [Rank] int, Email varchar(max),User_Name varchar(max), Password varchar(max), payment_id_info varchar(max));
 
 SELECT
     *
