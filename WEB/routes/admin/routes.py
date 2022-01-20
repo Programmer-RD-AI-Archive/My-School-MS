29032db44dcfe017702fd26f63353085d9d5c660
Programmer-RD-AI <aigithub@gmail.com>
2022-01-20 19:22:01 +0530

    Update routes.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- WEB/routes/admin/routes.py
+++ WEB/routes/admin/routes.py
@@ -6,4 +6,114 @@ from WEB.help_funcs import *
 @app.route("/Admin/")
 @app.route("/Admin")
 def admin_home():
-    pass
+    if "Is_Admin" in session:
+        return render_template("admin/home.html", session=session)
+
+
+@app.route("/Admin/Accounts")
+@app.route("/Admin/Accounts/")
+def admin_accounts():
+    if "Is_Admin" in session:
+        accounts = requests.get(
+            "http://127.0.0.1:5000/api/azure/sql",
+            {"Type": "Select", "Query": "SELECT * FROM Accounts"},
+        )
+        accounts = accounts.json()["message"]
+        return render_template("admin/accounts.html", accounts=accounts)
+
+
+@app.route("/Admin/Accounts/Edit/<_id>")
+@app.route("/Admin/Accounts/Edit/<_id>/")
+def admin_account_edit():
+    if "Is_Admin" in session:
+        # accounts = requests.get(
+        #     "http://127.0.0.1:5000/api/azure/sql",
+        #     {"Type": "Select", "Query": "SELECT * FROM Accounts"},
+        # )
+        # accounts = accounts.json()["message"]
+        flash("Not Working TODO", "success")  # TODO
+        return redirect("/Admin/Accounts")
+
+
+@app.route("/Admin/Accounts/Delete/<_id>")
+@app.route("/Admin/Accounts/Delete/<_id>/")
+def admin_account_delete(_id):
+    if "Is_Admin" in session:
+        accounts = requests.get(
+            "http://127.0.0.1:5000/api/azure/sql",
+            {"Type": "Insert", "Query": f"DELETE FROM Accounts WHERE ID={_id}; "},
+        )
+        accounts = accounts.json()["message"]
+        flash("Deleted", "success")
+        return redirect("/Admin/Accounts")
+
+
+@app.route("/Admin/Subjects", methods=["GET", "POST"])
+@app.route("/Admin/Subjects/", methods=["GET", "POST"])
+def admin_subjects():
+    if "Is_Admin" in session:
+        if request.method == "POST":
+            name = request.form["Name"]
+            description = request.form["Description"]
+            link = request.form["Link"]
+            subjects = requests.get(
+                "http://127.0.0.1:5000/api/azure/sql",
+                {
+                    "Type": "Insert",
+                    "Query": f"""
+                INSERT INTO
+                    [Subjects] ([Name],[Description],[Image])
+                VALUES
+                    (
+                       '{name}','{description}','{link}'
+                    );
+                """,
+                },
+            )
+            subjects = subjects.json()["message"]
+        subjects = requests.get(
+            "http://127.0.0.1:5000/api/azure/sql",
+            {"Type": "Select", "Query": f"SELECT * FROM Subjects"},
+        )
+        subjects = subjects.json()["message"]
+        return render_template("/admin/subjects.html", subjects=subjects)
+
+
+@app.route("/Admin/Subjects/Delete/<_id>", methods=["GET", "POST"])
+@app.route("/Admin/Subjects/Delete/<_id>/", methods=["GET", "POST"])
+def admin_subject_delete(_id):
+    if "Is_Admin" in session:
+        subjects = requests.get(
+            "http://127.0.0.1:5000/api/azure/sql",
+            {"Type": "Insert", "Query": f"DELETE FROM Subjects WHERE ID={_id}; "},
+        )
+        subjects = subjects.json()["message"]
+        flash("Deleted", "success")
+        return redirect("/Admin/Subjects")
+
+
+@app.route("/Admin/Subjects/Edit/<_id>/")
+@app.route("/Admin/Subjects/Edit/<_id>/")
+def admin_subject_edit(_id):
+    if "Is_Admin" in session:
+        # accounts = requests.get(
+        #     "http://127.0.0.1:5000/api/azure/sql",
+        #     {"Type": "Select", "Query": "SELECT * FROM Accounts"},
+        # )
+        # accounts = accounts.json()["message"]
+        flash("Not Working TODO", "success")  # TODO
+        return redirect("/Admin/Subjects")
+
+
+@app.route("/Admin/Log/Out")
+@app.route("/Admin/Log/Out/")
+def admin_log_out():
+    if "Is_Admin" in session:
+        session.pop("Is_Admin")
+        session.pop("id")
+        session.pop("User Name")
+        session.pop("Email")
+        session.pop("Rank")
+        session.pop("Password")
+        flash("Loged Out", "success")
+        return redirect("/")
