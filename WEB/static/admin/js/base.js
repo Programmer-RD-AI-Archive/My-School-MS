3eb2d5d3abec533f34b5f8e72c7223d25183cf0c
Programmer-RD-AI <aigithub@gmail.com>
2022-01-20 19:22:33 +0530

    Create base.js
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ WEB/static/admin/js/base.js
@@ -0,0 +1,17 @@
+/*
+sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+*/
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement(
+    {
+      pageLanguage: "en",
+      layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+    },
+    "google_translate_eMicrosoft Edgelement"
+  );
+}
