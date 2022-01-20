8bfe6ce137ff77dd090553406d75a2f7c239ff4d
Programmer-RD-AI <aigithub@gmail.com>
2022-01-20 11:15:27 +0530

    Create SQLQuickQuery.sql

---
+++ SQL/SQLQuickQuery.sql
@@ -0,0 +1,8 @@
+-- DROP TABLE Accounts;
+
+-- CREATE TABLE Accounts (ID int IDENTITY(1,1), [Rank] int, Email varchar(max),User_Name varchar(max), Password varchar(max), payment_id_info varchar(max));
+
+SELECT
+    *
+FROM
+    Accounts;
