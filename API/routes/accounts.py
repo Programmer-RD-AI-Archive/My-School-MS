f583234d7771a7c6d7d98aab365681e4538f5678
Programmer-RD-AI <aigithub@gmail.com>
2022-01-20 08:44:08 +0530

    Update accounts.py

--- API/routes/accounts.py
+++ API/routes/accounts.py
@@ -11,7 +11,10 @@ accounts_request_parser.add_argument(
 accounts_request_parser.add_argument(
     """user_name""", type=str, help="""user name is required""", required=True
 )
-accounts_request_parser.add_argument("""password_hash""", type=str, required=True)
+# accounts_request_parser.add_argument("""password_hash""", type=str, required=True)
+accounts_request_parser.add_argument(
+    """payment_id_info""", type=str, help="""payment_id_info is required""", required=True
+)
 
 
 class Accounts(Resource):
@@ -30,9 +33,10 @@ class Accounts(Resource):
         Return: return_description
         """
         asql = Azure_SQL()
+
         hp.table_exists_or_not(
             """Accounts""",
-            """CREATE TABLE Accounts (ID int IDENTITY(1,1), Rank INT(max), Email varchar(max),User_Name varchar(max), Password varchar(max))""",
+            """CREATE TABLE Accounts (ID int IDENTITY(1,1), Rank INT(max), Email varchar(max),User_Name varchar(max), Password varchar(max), payment_id_info varchar(max))""",
         )
         newaccounts = []
         accounts = asql.select_table("""SELECT * FROM [Accounts]""")
@@ -49,17 +53,29 @@ class Accounts(Resource):
         """
         args = accounts_request_parser.parse_args()
         asql = Azure_SQL()
+        astorage = Azure_Storage("account")
+        ids_of_accounts = []
+        for account in accounts:
+            ids_of_accounts.append(account)
+        if ids_of_accounts == []:
+            ids_of_accounts = [[0]]
+        id_new = ids_of_accounts[-1][0]
+        id_new += 1
+        info = str(args["payment_id_info"])
+        info = bytes(info, encoding="utf-8")
+        astorage.create_file(file_name_in_the_cloud=f"{id_new}-info.txt", file_rb=info)
         hp.table_exists_or_not(
             """Accounts""",
-            """CREATE TABLE Accounts (ID int IDENTITY(1,1), Rank INT(max), Email varchar(max),User_Name varchar(max), Password varchar(max))""",
+            """CREATE TABLE Accounts (ID int IDENTITY(1,1), Rank INT(max), Email varchar(max),User_Name varchar(max), Password varchar(max), payment_id_info varchar(max))""",
         )
         asql.insert_to_table(
-            f"""INSERT INTO [Accounts]( [Rank],[Email], [User_Name], [Password] ) VALUES ( 1,'{args['email']}', '{args['user_name']}', '{args['password']}')"""
+            f"""INSERT INTO [Accounts]( [Rank],[Email], [User_Name], [Password], [payment_id_info] ) VALUES ( 1,'{args['email']}', '{args['user_name']}', '{args['password']}','{id_new}-info.txt')"""
         )
         newaccounts = []
         accounts = asql.select_table("""SELECT * FROM [Accounts]""")
         for account in accounts:
             newaccounts.append(list(account))
+        accounts = asql.select_table("SELECT * FROM Accounts")
         return {"""message""": newaccounts}
 
 
