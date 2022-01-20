925af2bf1d3347f2661adfbf874e81e171cf3679
Programmer-RD-AI <aigithub@gmail.com>
2022-01-20 11:15:13 +0530

    Update azure_storage.py

--- API/db/azure_storage.py
+++ API/db/azure_storage.py
@@ -20,13 +20,13 @@ class Azure_Storage:
         """
         self.connection_str = """DefaultEndpointsProtocol=https;AccountName=helpyoulearnstuff;AccountKey=WMruG6IqnwGspaRB9vIL+SmhTwzM3iPE7cRtjHkikxpa7WJo5EvQ+rIqjFZIgoPqwmEvOCZ/4KSf42yVX8kkQQ==;EndpointSuffix=core.windows.net"""
         self.blob_service_client = BlobServiceClient.from_connection_string(
-            conn_str=self.connection_str)
+            conn_str=self.connection_str
+        )
         self.container_name = str(container_name)
         try:
-            self.container_client = self.blob_service_client.create_container(
-                self.container_name)
+            self.container_client = self.blob_service_client.create_container(self.container_name)
         except Exception as e:
-            warnings.filterwarnings(e)
+            pass
 
     def create_file(self, file_rb, file_name_in_the_cloud: str) -> None:
         """sumary_line
@@ -36,7 +36,8 @@ class Azure_Storage:
         Return: return_description
         """
         blob_client = self.blob_service_client.get_blob_client(
-            container=self.container_name, blob=file_name_in_the_cloud)
+            container=self.container_name, blob=file_name_in_the_cloud
+        )
         blob_client.upload_blob(file_rb, overwrite=True)
 
     def find_file(self) -> None:
@@ -61,7 +62,8 @@ class Azure_Storage:
         Return: return_description
         """
         blob_client = self.blob_service_client.get_blob_client(
-            container=self.container_name, blob=file_name_in_the_cloud)
+            container=self.container_name, blob=file_name_in_the_cloud
+        )
         return blob_client.download_blob().readall()
 
     def delete_blob(self) -> None:
