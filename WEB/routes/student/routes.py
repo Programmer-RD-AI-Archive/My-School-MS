d263c98b5a472bbec1a39b3cd6e94d891a57cf8c
Programmer-RD-AI <aigithub@gmail.com>
2022-01-20 12:22:42 +0530

    Update routes.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ WEB/routes/student/routes.py
@@ -0,0 +1,15 @@
+import warnings
+from WEB import *
+from WEB.help_funcs import *
+
+
+@app.route("/Usr/<_id>", methods=["GET", "POST"])
+def home(_id):
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+    if session["id"] == _id:
+        return render_template("student/home.html")
