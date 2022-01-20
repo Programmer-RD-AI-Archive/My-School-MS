4bc850b8d3491335499be8daddd49d71c5c3cc09
Programmer-RD-AI <aigithub@gmail.com>
2022-01-20 13:09:55 +0530

    Update SQLQuickQuery.sql
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- SQL/SQLQuickQuery.sql
+++ SQL/SQLQuickQuery.sql
@@ -1,8 +1,15 @@
 -- DROP TABLE Accounts;
 
 -- CREATE TABLE Accounts (ID int IDENTITY(1,1), [Rank] int, Email varchar(max),User_Name varchar(max), Password varchar(max), payment_id_info varchar(max));
+INSERT INTO
+    [Accounts] ([Rank],[Email], [User_Name], [Password])
+VALUES
+    (
+       5,'go2ranuga@gmail.com','Ranuga','UmFudWdh'
+    );
 
 SELECT
     *
 FROM
     Accounts;
+
