9c80f8502f04444b62f1b3af17060f8cf48fb7a9
Programmer-RD-AI <aigithub@gmail.com>
2022-01-21 23:51:33 +0530

    Update SQLQuery.sql
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- SQL/SQLQuery.sql
+++ SQL/SQLQuery.sql
@@ -21,17 +21,22 @@ FROM
 DROP TABLE Accounts;
 
 CREATE TABLE Accounts (ID int IDENTITY(1,1), [Rank] int, Email varchar(max),User_Name varchar(max), Password varchar(max), payment_id_info varchar(max));
-
+INSERT INTO
+    [Accounts] ([Rank],[Email], [User_Name], [Password])
+VALUES
+    (
+       5,'go2ranuga@gmail.com','Ranuga','UmFudWdh'
+    );
 SELECT
     *
 FROM
     Accounts;
 
 -- All of the tables
-SELECT
-    [table_name]
-FROM
-    [information_schema.tables];
+-- SELECT
+--     [table_name]
+-- FROM
+--     [information_schema.tables];
 
 -- Questions
 DROP TABLE Questions;
@@ -65,16 +70,20 @@ FROM
     Resources;
 
 -- Courses
-DROP TABLE Courses;
 
-CREATE TABLE Courses (
-    [ID] int IDENTITY(1, 1),
+DROP TABLE Courses;
+CREATE TABLE Courses
+(
+    [ID] int IDENTITY(1,1),
     [Whole_Content] varchar(max),
     [Info] varchar(max),
     [Image] varchar(max),
     [Name] varchar(max),
-    [Marks] varchar(max) -- [Description] varchar(max)
-);
+    [Marks] varchar(max),
+    [Description] varchar(max),
+    [Subject] varchar(max),
+    [Tutor ID] varchar(max)
+)
 
 SELECT
     *
@@ -84,16 +93,47 @@ FROM
 -- TEST
 DROP TABLE [TEST];
 
-CREATE TABLE [TEST] ([Testing_Object] varchar(max));
+-- CREATE TABLE [TEST] ([Testing_Object] varchar(max));
 
-INSERT INTO
-    [TEST] ([Testing_Object])
-VALUES
-    (
-        '{"1": [["te", "3", "question"]], "2": [["te", "3", "question"]]}'
-    );
+-- INSERT INTO
+--     [TEST] ([Testing_Object])
+-- VALUES
+--     (
+--         '{"1": [["te", "3", "question"]], "2": [["te", "3", "question"]]}'
+--     );
 
 SELECT
     *
 FROM
     [TEST];
+-- Subjects
+DROP TABLE Subjects
+CREATE TABLE Subjects (ID int IDENTITY(1,1), [Name] varchar(max), [Description] varchar(max), [Image] varchar(max))
+-- Tutor
+
+DROP TABLE Tutor
+CREATE TABLE Tutor 
+(
+    ID int IDENTITY(1,1), 
+    [Email] varchar(max), 
+    [Password] varchar(max), 
+    [User_Name] varchar(max), 
+    [Description] varchar(max), 
+    [Qualification] varchar(max), 
+    [Profile Picture] varchar(max), 
+    [Full Name] varchar(max), 
+    [Contact Number] varchar(max), 
+    [Rating] varchar(max),
+    [Enabled] varchar(max)
+);
+-- Enrolled
+DROP TABLE Enrolled;
+CREATE TABLE Enrolled (
+    [ID] int IDENTITY(1,1),
+    [Student Id] int,
+    [Course Id] int,
+    [Tutor Id] int,
+    [Current Lesson] int,
+    [Total Lesson] int,
+    [Paid] varchar(max),
+)
