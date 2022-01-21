2101aeec67d3dc11bd80ccff21597911b5d11f8b
Programmer-RD-AI <aigithub@gmail.com>
2022-01-21 08:31:56 +0530

    Update SQLQuickQuery.sql
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- SQL/SQLQuickQuery.sql
+++ SQL/SQLQuickQuery.sql
@@ -16,3 +16,22 @@ FROM
 
 -- CREATE TABLE Subjects (ID int IDENTITY(1,1), [Name] varchar(max), [Description] varchar(max), [Image] varchar(max))
 SELECT * FROM Subjects;
+
+-- CREATE TABLE Courses (ID int IDENTITY(1,1), [Title] varchar(max), [Description] varchar(max), [Image] varchar(max),[Content] varchar(max))
+-- DROP TABLE Tutor
+-- CREATE TABLE Tutor 
+-- (
+--     ID int IDENTITY(1,1), 
+--     [Email] varchar(max), 
+--     [Password] varchar(max), 
+--     [User_Name] varchar(max), 
+--     [Description] varchar(max), 
+--     [Qualification] varchar(max), 
+--     [Profile Picture] varchar(max), 
+--     [Full Name] varchar(max), 
+--     [Contact Number] varchar(max), 
+--     [Rating] varchar(max),
+--     [Enabled] varchar(max)
+-- );
+
+SELECT * FROM Tutor
