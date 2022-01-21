855de2940b5d41a780650830eb37da2f598eefec
Programmer-RD-AI <aigithub@gmail.com>
2022-01-21 13:02:59 +0530

    Update routes.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- WEB/routes/student/routes.py
+++ WEB/routes/student/routes.py
@@ -28,7 +28,7 @@ def student_home(_id):
             iter_list.append(subject)
             idx += 1
         new_subjects.append(iter_list)
-        return render_template("student/home.html", subjects=new_subjects, session=session)
+        return render_template("student/home.html", subjects=new_subjects, session=session, _id=_id)
 
 
 @app.route("/Usr/<_id>/Subject/<name_of_subject>/", methods=["GET", "POST"])
@@ -46,4 +46,42 @@ def student_subjects(_id, name_of_subject):
             {"Type": "Select", "Query": f"SELECT * FROM Courses WHERE Subject={name_of_subject}"},
         )
         courses = courses.json()["message"]
-        return render_template("student/subject.html", courses=courses)
+        iter_list = []
+        new_courses = []
+        idx = 0
+        for course in courses:
+            if idx % 3 == 0:
+                new_courses.append(iter_list)
+                iter_list = []
+            iter_list.append(course)
+            idx += 1
+        new_courses.append(iter_list)
+        return render_template("student/subject.html", courses=new_courses, _id=_id)
+
+
+@app.route("/Usr/<_id>/Subject/<name_of_subject>/Enroll", methods=["GET", "POST"])
+@app.route("/Usr/<_id>/Subject/<name_of_subject>/Enroll/", methods=["GET", "POST"])
+def student_subject_enroll(_id, name_of_subject):
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+    if str(session["id"]) == str(_id):
+        courses = requests.get(
+            "http://127.0.0.1:5000/api/azure/sql",
+            {"Type": "Select", "Query": f"SELECT * FROM Courses WHERE Subject={name_of_subject}"},
+        )
+        courses = courses.json()["message"]
+        iter_list = []
+        new_courses = []
+        idx = 0
+        for course in courses:
+            if idx % 3 == 0:
+                new_courses.append(iter_list)
+                iter_list = []
+            iter_list.append(course)
+            idx += 1
+        new_courses.append(iter_list)
+        return render_template("student/subject.html", courses=new_courses)
