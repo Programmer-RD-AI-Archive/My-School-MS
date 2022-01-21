7023d8a716af676a2016c5f5460aded60e941b80
Programmer-RD-AI <aigithub@gmail.com>
2022-01-21 13:02:36 +0530

    Update __init__.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- API/routes/__init__.py
+++ API/routes/__init__.py
@@ -2,4 +2,7 @@ from API.routes.accounts import *
 from API.routes.azure_sql_api import *
 from API.routes.azure_storage_api import *
 from API.routes.contact_us import *
+from API.routes.courses import *
 from API.routes.get_config import *
+from API.routes.questions import *
+from API.routes.resources import *
