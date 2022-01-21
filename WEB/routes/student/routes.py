8b61290e339fe83f186ffb611f74cc332d80d7b2
Programmer-RD-AI <aigithub@gmail.com>
2022-01-21 08:32:06 +0530

    Update routes.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- WEB/routes/student/routes.py
+++ WEB/routes/student/routes.py
@@ -28,5 +28,22 @@ def student_home(_id):
             iter_list.append(subject)
             idx += 1
         new_subjects.append(iter_list)
-        return render_template("student/home.html", subjects=new_subjects)
-    return "zgdrghdr"
+        return render_template("student/home.html", subjects=new_subjects, session=session)
+
+
+@app.route("/Usr/<_id>/Subject/<name_of_subject>/", methods=["GET", "POST"])
+@app.route("/Usr/<_id>/Subject/<name_of_subject>", methods=["GET", "POST"])
+def student_subjects(_id, name_of_subject):
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+    if str(session["id"]) == str(_id):
+        courses = requests.get(
+            "http://127.0.0.1:5000/api/azure/sql",
+            {"Type": "Select", "Query": f"SELECT * FROM Courses WHERE Subject={name_of_subject}"},
+        )
+        courses = courses.json()["message"]
+        return render_template("student/subject.html", courses=courses)
