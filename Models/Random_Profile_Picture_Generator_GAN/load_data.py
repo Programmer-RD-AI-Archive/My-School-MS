0a1cbda86f21420d90c05a5256b7b469785c5af7
Programmer-RD-AI <aigithub@gmail.com>
2022-01-23 00:24:41 +0530

    Create load_data.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ Models/Random_Profile_Picture_Generator_GAN/load_data.py
@@ -0,0 +1,12 @@
+from Models.Random_Profile_Picture_Generator_GAN import *
+
+
+def load_data(directory="./data/", IMG_SIZE=56, transforms=transforms):
+    idx = -1
+    data = []
+    for file in tqdm(os.listdir(directory)):
+        file = directory + file
+        img = cv2.imread(file, cv2.IMREAD_COLOR)
+        img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+        data.append(np.array(transforms(np.array(img))))
+    return data
