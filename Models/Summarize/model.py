3502627ebdfa6b39adf8abb500a24b4609d8951d
Programmer-RD-AI <aigithub@gmail.com>
2022-01-23 00:27:43 +0530

    Create model.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ Models/Summarize/model.py
@@ -0,0 +1,19 @@
+from Models.Summarize import *
+
+
+class Model(Module):
+    def __init__(self):
+        super().__init__()
+        self.activation = ReLU()
+        self.iters = 10
+        self.linear1 = Linear(len(X_words),256)
+        self.linear2 = Linear(256,256)
+        self.linear2bn = BatchNorm1d(256)
+        self.output = Linear(256,len(y_words))
+    
+    def forward(self,X):
+        preds = self.linear1(X)
+        for _ in range(self.iters):
+            preds = self.activation(self.linear2bn(self.linear2(preds)))
+        preds = self.output(preds)
+        return preds
