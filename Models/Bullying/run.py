54e9c8817ce6440bb93436aef25de3bc6fb7d2c7
Programmer-RD-AI <aigithub@gmail.com>
2022-01-23 00:23:02 +0530

    Create run.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ Models/Bullying/run.py
@@ -0,0 +1,13 @@
+from Models.Bullying import *
+
+X_train, X_test, y_train, y_test = load_data()
+model = Model().to(device)
+criterion = MSELoss()
+optimizer = Adam(model.parameters(), lr=0.001)
+epochs = 100
+batch_size = 8
+train(epochs, X_train, y_train, X_test, y_test, model, criterion, optimizer, batch_size)
+torch.save(model, "model.pt")
+torch.save(model, "model.pth")
+torch.save(model.state_dict(), "model-sd.pt")
+torch.save(model.state_dict(), "model-sd.pth")
