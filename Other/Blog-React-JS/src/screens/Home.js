267cb146b6ecb1101cc6ea84792fc39c07f9db62
Programmer-RD-AI <aigithub@gmail.com>
2022-01-23 00:30:19 +0530

    Create Home.js
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ Other/Blog-React-JS/src/screens/Home.js
@@ -0,0 +1,20 @@
+import NavBar from "../components/NavBar";
+
+const Home = () => {
+  return (
+    <>
+
+        <center>
+          <div className="center_text">
+            <div className="company_name">blog stories</div>
+          </div>
+          <div className="center_text" style={{ marginTop: 125 }}>
+            <div className="moto">Like Reading Stories ?</div>
+          </div>
+        </center>
+ 
+    </>
+  );
+};
+
+export default Home;
