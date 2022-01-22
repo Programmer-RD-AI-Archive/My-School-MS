7632e9222dddad452fd7ecef638e0fac71727a2a
Programmer-RD-AI <aigithub@gmail.com>
2022-01-23 00:26:15 +0530

    Create metrics.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ Models/Random_Profile_Picture_Generator_GAN/metrics.py
@@ -0,0 +1,23 @@
+from Models.Random_Profile_Picture_Generator_GAN import *
+
+
+def accuracy_fake(desc_fake):
+    correct = 0
+    total = 0
+    preds = np.round(np.array(desc_fake.cpu().detach().numpy()))
+    for pred in preds:
+        if pred == 0:
+            correct += 1
+        total += 1
+    return round(correct / total, 3)
+
+
+def accuracy_real(desc_real):
+    correct = 0
+    total = 0
+    preds = np.round(np.array(desc_real.cpu().detach().numpy()))
+    for pred in preds:
+        if pred == 1:
+            correct += 1
+        total += 1
+    return round(correct / total, 3)
