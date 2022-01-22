3ca2d73d3cc7127fb6d5394fe73e8e4c72700ab1
Programmer-RD-AI <aigithub@gmail.com>
2022-01-23 00:26:46 +0530

    Create cnn.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ Models/Rock_Paper_Scissors/models/cnn.py
@@ -0,0 +1,39 @@
+from Models.Rock_Paper_Scissors import *
+
+
+class Model(Module):
+    def __init__(self):
+        super().__init__()
+        self.max_pool2d = MaxPool2d((2, 2), (2, 2))
+        self.activation = ReLU()
+        self.conv1 = Conv2d(3, 6, (5, 5))
+        self.conv1batchnorm = BatchNorm2d(6)
+        self.conv2 = Conv2d(6, 9, (5, 5))
+        self.conv2batchnorm = BatchNorm2d(9)
+        self.conv3 = Conv2d(9, 12, (5, 5))
+        self.conv3batchnorm = BatchNorm2d(12)
+        self.conv4 = Conv2d(12, 15, (5, 5))
+        self.conv4batchnorm = BatchNorm2d(15)
+        self.linear1 = Linear(15 * 3 * 3, 512)
+        self.linear1batchnorm = BatchNorm1d(512)
+        self.linear2 = Linear(512, 1024)
+        self.linear2batchnorm = BatchNorm1d(1024)
+        self.linear3 = Linear(1024, 2048)
+        self.linear3batchnorm = BatchNorm1d(2048)
+        self.linear4 = Linear(2048, 1024)
+        self.linear4batchnorm = BatchNorm1d(1024)
+        self.output = Linear(1024, 3)
+
+    def forward(self, X):
+        preds = self.max_pool2d(self.activation(self.conv1batchnorm(self.conv1(X))))
+        preds = self.max_pool2d(self.activation(self.conv2batchnorm(self.conv2(preds))))
+        preds = self.max_pool2d(self.activation(self.conv3batchnorm(self.conv3(preds))))
+        preds = self.max_pool2d(self.activation(self.conv4batchnorm(self.conv4(preds))))
+        #         print(preds.shape)
+        preds = preds.view(-1, 15 * 3 * 3)
+        preds = self.activation(self.linear1batchnorm(self.linear1(preds)))
+        preds = self.activation(self.linear2batchnorm(self.linear2(preds)))
+        preds = self.activation(self.linear3batchnorm(self.linear3(preds)))
+        preds = self.activation(self.linear4batchnorm(self.linear4(preds)))
+        preds = self.output(preds)
+        return preds
