3e4d049f47ca6a6c29c13bd5effb28d5134cd9a2
Programmer-RD-AI <aigithub@gmail.com>
2022-01-23 00:27:00 +0530

    Create metrics.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ Models/Sign_Language_Recognition/metrics.py
@@ -0,0 +1,30 @@
+from Models.Sign_Language_Recognition import *
+
+
+def test(net, X, y):
+    device = "cpu"
+    net.to(device)
+    correct = 0
+    total = 0
+    net.eval()
+    with torch.no_grad():
+        for i in range(len(X)):
+            real_class = torch.argmax(y[i]).to(device)
+            net_out = net(X[i].view(-1, 1, 112, 112).to(device).float())
+            net_out = net_out[0]
+            predictied_class = torch.argmax(net_out)
+            if predictied_class == real_class:
+                correct += 1
+            total += 1
+    net.train()
+    net.to("cuda")
+    return round(correct / total, 3)
+
+
+def get_loss(criterion, y, model, X):
+    model.to("cpu")
+    preds = model(X.view(-1, 1, 112, 112).to("cpu").float())
+    preds.to("cpu")
+    loss = criterion(preds, torch.tensor(y, dtype=torch.long).to("cpu"))
+    loss.backward()
+    return loss.item()
