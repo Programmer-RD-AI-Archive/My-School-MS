d914bdc8c4961f31df77a8d6431feeaa09a3e312
Programmer-RD-AI <aigithub@gmail.com>
2022-01-23 11:39:51 +0530

    Update sign_in.js
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- WEB/static/home/js/sign_in.js
+++ WEB/static/home/js/sign_in.js
@@ -6,6 +6,12 @@ argument -- description
 Return: return_description
 */
 function onChange() {
+  /**
+   * Function to add two numbers
+   * @param a The first number to add
+   * @param b The second number to add
+   * @returns The sum of two numbers
+   */
   const password = document.querySelector("input[name=password]");
   const confirm = document.querySelector("input[name=confirm]");
   if (confirm.value === password.value) {
