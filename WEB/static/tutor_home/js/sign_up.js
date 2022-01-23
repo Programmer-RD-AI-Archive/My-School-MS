801bcc6c6e1e3e53b20ae29aaf5c0e97bcd9e45d
Programmer-RD-AI <aigithub@gmail.com>
2022-01-23 11:40:21 +0530

    Update sign_up.js
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- WEB/static/tutor_home/js/sign_up.js
+++ WEB/static/tutor_home/js/sign_up.js
@@ -6,6 +6,12 @@ argument -- description
 Return: return_description
 */
 function onChange() {
+  /**
+   * Function to add two numbers
+   * @param a The first number to add
+   * @param b The second number to add
+   * @returns The sum of two numbers
+   */
   const password = document.querySelector("input[name=Password]");
   const confirm = document.querySelector("input[name=Confirm Password]");
   if (confirm.value === password.value) {
