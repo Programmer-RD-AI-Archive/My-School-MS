b4973cdb857d61c299db55e8883953f9c6eeafb3
Programmer-RD-AI <aigithub@gmail.com>
2022-01-23 11:39:25 +0530

    Update reportWebVitals.js
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- Other/Messenger/src/reportWebVitals.js
+++ Other/Messenger/src/reportWebVitals.js
@@ -6,6 +6,12 @@ argument -- description
 Return: return_description
 */
 const reportWebVitals = (onPerfEntry) => {
+  /**
+   * Function to add two numbers
+   * @param a The first number to add
+   * @param b The second number to add
+   * @returns The sum of two numbers
+   */
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
