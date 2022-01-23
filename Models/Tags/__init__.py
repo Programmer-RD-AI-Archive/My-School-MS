d067dc44dfc6f6db2a4a7a07495313cbe2a8bf25
Programmer-RD-AI <aigithub@gmail.com>
2022-01-23 13:14:15 +0530

    Create __init__.py
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ Models/Tags/__init__.py
@@ -0,0 +1,21 @@
+import random
+
+import nltk
+import numpy as np
+import pandas as pd
+import torch
+import torchvision
+import wandb
+from nltk.stem.porter import *
+from torch.nn import *
+from torch.optim import *
+from torch.utils.data import DataLoader, Dataset
+from tqdm import *
+
+from Models.Summarize_V2.help_funcs import *
+from Models.Summarize_V2.metrics import *
+from Models.Summarize_V2.model import *
+
+device = "cuda"
+PROJECT_NAME = "Tag"
+stemmer = PorterStemmer()
