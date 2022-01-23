5ebb1035bc90e6c2aa20e6edb915185804d2e2b7
Programmer-RD-AI <aigithub@gmail.com>
2022-01-23 11:35:33 +0530

    Update reducer.js
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

--- Other/Whats-App/src/reducer.js
+++ Other/Whats-App/src/reducer.js
@@ -12,6 +12,12 @@ export const actionTypes = {
   SET_USER: "SET_USER",
 };
 const reducer = (state, action) => {
+  /**
+   * Function to add two numbers
+   * @param a The first number to add
+   * @param b The second number to add
+   * @returns The sum of two numbers
+   */
   console.log(action);
   console.log("State ", state);
   switch (action.type) {
